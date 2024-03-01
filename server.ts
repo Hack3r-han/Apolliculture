2c2e9127db04ac3fba8d616f90122295de3c4e12
DESKTOP-G02BKC3\Laudyciana <laudyeneth@hotmail.com>
2024-03-01 14:05:52 +0100

    Dependencies, tsconfig and server are added

---
+++ server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+// import UsersRouter from './routes/UsersRouter.js';
+// import ProductsRouter from './routes/ProductsRouter.js';
+// import CategoriesRouter from './routes/CategoriesRouter.js';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json()) 
+
+// app.use("/users/", UsersRouter);
+
+// app.use("/products/", ProductsRouter);
+
+// app.use("/categories/", CategoriesRouter);
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
