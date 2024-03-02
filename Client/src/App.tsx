c294af50a6dd83a2848b4be1c18c1c2fe2ebf43d
Yadyvivian <148440973+Yadyvivian@users.noreply.github.com>
2024-03-02 14:10:09 +0100

    change in router

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,28 +1,5 @@
-// export default function App() {
-//   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-// }
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
 
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-// import HoneyComponent from './components/HoneyComponent';
-// import WaxComponent from './components/WaxComponent';
-// import MeadComponent from './components/MeadComponent';
-// import OtherServicesComponent from './components/OtherServicesComponent';
-
-const App: React.FC = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        {/* <Route path="/honey" component={HoneyComponent} />
-        <Route path="/wax" component={WaxComponent} />
-        <Route path="/mead" component={MeadComponent} />
-        <Route path="/other-services" component={OtherServicesComponent} /> */}
-      </Switch>
-    </Router>
-  );
-};
-
-export default App;
