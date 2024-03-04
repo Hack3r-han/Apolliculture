e1f0f20fee66a544278f8c4bc64784289f8cc864
Isarok <isaac.lucerito@gmail.com>
2024-03-04 15:13:47 +0100

    add merge feature/footer

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -4,14 +4,11 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-      
-=======
        <div className="min-h-screen flex flex-col">  
             <video src={video}/>
             <Footer />
         </div>
->>>>>>> feature/footer
+
     </>
   )
 }
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
 import App from './App'
-=======
-import App from './App.tsx'
->>>>>>> feature/footer
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-<<<<<<< HEAD
     <App/>
-=======
-    <App />
->>>>>>> feature/footer
   </React.StrictMode>,
 )
