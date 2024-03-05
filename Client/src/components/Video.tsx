e4df2ff02618d128ca28e4a5300c193cb618072d
Isarok <isaac.lucerito@gmail.com>
2024-03-05 09:01:02 +0100

    add styles to combine

--- Client/src/components/Video.tsx
+++ Client/src/components/Video.tsx
@@ -10,16 +10,20 @@ function Video ({ src }: { src: string }) {
   };
 
   return (
-    <header className="relative w-full h-screen overflow-hidden" onClick={toggleMute}>
+/*     <footer className="md:ml-auto flex flex-colum justify-center  relative w-full h-screen overflow-hidden" onClick={toggleMute}>
       <div className="absolute inset-0 flex items-center justify-center">
         <video id="video" src={src} className="absolute inset-0 object-cover w-full max-w-full " autoPlay loop muted />
       </div>
-      <div className="absolute inset-x-0 bottom-0 z-10 py-4">
-        <div className="container mx-auto text-center text-white">
-          
+      <div className="absolute inset-x-0 bottom-0 z-10 py-4"></div>
+    </footer>
+  ); */
+    <div className="video-container relative">
+      <header className="w-full h-screen overflow-hidden" onClick={toggleMute}>
+        <div className="absolute inset-0 flex items-center justify-center">
+          <video id="video" src={src} className="absolute inset-0 object-cover w-full max-w-full h-full" autoPlay loop muted />
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 }
 
