46635480ead155bea58d618a656d00c99ffafb92
Yadyvivian <148440973+Yadyvivian@users.noreply.github.com>
2024-03-08 15:27:42 +0100

    add modal

--- Client/src/router/router.tsx
+++ Client/src/router/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom"
 import Home from '../pages/Home'
 import Loginregister from "../pages/Loginregister"
 import Dashboard from "../pages/Dashboard"
+import Modal from "../pages/Modal"
 import Root from './Root'
 
 
