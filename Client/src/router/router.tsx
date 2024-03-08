ed206c7332e78b2362031509b2e988e5b11b737e
Isarok <isaac.lucerito@gmail.com>
2024-03-08 09:32:26 +0100

    add router

---
+++ Client/src/router/router.tsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom"
+import Home from '../pages/Home'
+import Loginregister from "../pages/Loginregister"
+import Dashboard from "../pages/Dashboard"
+import Root from './Root'
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        children: [
+            {
+                path: "/home",
+                element: <Home />,
+               
+            },
+            {
+                path: "/Dashboard",
+                element: <Dashboard />,
+                
+            },
+            {
+                path: "/Loginregister",
+                element: <Loginregister />,
+            }
+
+        ],
+    },
+]);
