760d7cc02034bd714e7ad7e736cecbc5e574b26f
Hack3r-han <hannahphil27@gmail.com>
2024-03-09 15:55:12 +0100

    Adapt the style of the header of the homepage

--- Client/src/components/Header.tsx
+++ Client/src/components/Header.tsx
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <div className="mb-5">
       <img src={headerImage} alt="Header" className="w-full h-auto" />
-      <h1 className="text-center text-3xl font-bold">DISCOVER WHY WE'RE THE BEES KNEES</h1>
-      <p className="text-center mb-5">
+      <h1 className="text-center text-3xl font-bold mt-10">DISCOVER WHY WE'RE THE BEES KNEES</h1>
+      <p className="text-center my-10">
         Welcome to Apolliculture. Inspired by the radiant legacy of the Greek God Apollo, we cultivate a vibrant culture for bee enthusiasts, blending sustainability with divine craftsmanship. Discover our sun-kissed collection of bee-inspired treasures, from pure, artisan honey to beeswax candles.
       </p>
     </div>
