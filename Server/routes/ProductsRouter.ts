32bdbebd905be800fbe673fde578642e9e689566
Hack3r-han <hannahphil27@gmail.com>
2024-03-20 23:07:48 +0100

    Open up the permissions to users for adding products

--- Server/routes/ProductsRouter.ts
+++ Server/routes/ProductsRouter.ts
@@ -6,9 +6,7 @@ const ProductsRouter = Router();
 
 ProductsRouter.route("/").get(ProductsController.getAllProducts);
 ProductsRouter.route("/:id").get(ProductsController.getProduct);
-
-//proteccion de conexion
-ProductsRouter.route("/").post(isAdmin,ProductsController.addProduct);
+ProductsRouter.route("/").post(ProductsController.addProduct);
 ProductsRouter.route("/:id").put(ProductsController.updateProduct);
 ProductsRouter.route("/:id").delete(ProductsController.deleteProduct);
 
